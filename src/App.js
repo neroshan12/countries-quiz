f0a4f22bae94086dd0a851029471b6fdd4c4ec74
Nero <neroshan12@gmail.com>
2021-11-01 12:29:13 +0000

    Set up Apollo and first graphql query

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import "./App.css";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import Home from "./containers/Home";
 
-function App() {
+const client = new ApolloClient({
+  uri: "https://countries.trevorblades.com/",
+  cache: new InMemoryCache(),
+});
+
+const App = () => {
   return (
-    <div className="App">
-      <Home></Home>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <Home />
+      </div>
+    </ApolloProvider>
   );
-}
+};
 
 export default App;
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,9 +1,22 @@
-function Home() {
+import React, { useState } from "react";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_COUNTRIES = gql`
+  query {
+    countries {
+      name
+    }
+  }
+`;
+
+const Home = () => {
+  const { loading, error, data } = useQuery(GET_COUNTRIES);
+  console.log(data);
   return (
-    <div>
+    <>
       <h1>Countries Quiz</h1>
-    </div>
+    </>
   );
-}
+};
 
 export default Home;
